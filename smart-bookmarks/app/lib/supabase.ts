5b9574b3820ff8f404b06559802e69f0eb668cf0
KalyanKumar-4 <kalyankumar062001@gmail.com>
2026-02-16 03:47:02 +0530

    Update supabase.ts
    secrets to env

--- smart-bookmarks/app/lib/supabase.ts
+++ smart-bookmarks/app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    'https://fpszatpekdshaviwagmu.supabase.co',
-    'sb_publishable_qbvdY_emF2NfNo_xZ3FxoQ_J1kQSk4M'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
